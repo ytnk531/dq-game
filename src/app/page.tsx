558806d900602be57470e8284ff1fe5dd977dfda
ytnk531 <ytnk531@gmail.com>
2023-11-05 08:31:43 +0900

    complicated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,147 @@
-import Image from 'next/image'
+'use client';
+
+import React, {useEffect, useRef, useState} from "react";
+import NextImage from "next/image";
+enum Action {
+    Fight,
+    Escape,
+}
+enum ControlState {
+    selectAction,
+    readMessage,
+}
+
+enum GameState {
+    fighting,
+    destroyed,
+    cleared
+}
 
 export default function Home() {
+    const view = { height: 600, width: 800 };
+    const [x, setX] = useState(100);
+    const [y, setY] = useState(100);
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const [image, setImage] = useState<HTMLImageElement | null>(null)
+    const [selectedAction, setSelectedAction] = useState(Action.Fight)
+    const [message, setMessage] = useState<string | null>(null)
+    const [e, setE] = useState<KeyboardEvent | null>(null)
+    const [controlState, setControlState] = useState(ControlState.selectAction)
+    const [enemyHealth, setEnemyHealth] = useState(5000)
+    const [gameState, setGamestate] = useState(GameState.fighting)
+
+    function listenKeyDown(e: KeyboardEvent) {
+        setE(e)
+    }
+    function act() {
+        if (controlState === ControlState.selectAction) {
+            if (selectedAction === Action.Fight) {
+                const damage = 800 + Math.floor(Math.random() * 100)
+                setMessage(`スライムに${damage}のダメージ`)
+                setEnemyHealth((enemyHealth) => enemyHealth - damage)
+                if (enemyHealth <= 0) {
+                    setGamestate(GameState.destroyed)
+                }
+                setControlState(ControlState.readMessage)
+            } else {
+                setMessage('しかしにげることはできない')
+                setControlState(ControlState.readMessage)
+            }
+        } else if (controlState === ControlState.readMessage) {
+            if (message) { setMessage(null) }
+            if (gameState === GameState.destroyed) {
+                setMessage('スライムをたおした！')
+                setControlState(ControlState.readMessage)
+                setGamestate(GameState.cleared)
+            } else if (gameState === GameState.cleared) {
+                // do nothing
+            } else {
+                setControlState(ControlState.selectAction)
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (!e) { return }
+
+        if (e.key === 'ArrowRight') {
+            setX((x) => x + 10);
+        }
+        if (e.key === 'ArrowLeft') {
+            setX((x) => x - 10);
+        }
+        if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+            setSelectedAction((selectedAction) => {
+                if (selectedAction === Action.Fight) {
+                    return Action.Escape
+                } else {
+                    return Action.Fight
+                }
+            })
+        }
+        if (e.key === 'Enter') {
+            act();
+        }
+    }, [e]);
+
+    function drawActions(ctx: CanvasRenderingContext2D, selectedAction: Action) {
+        console.log('drawActions', selectedAction)
+        ctx.strokeRect(180, 400, 150, 180)
+        if (selectedAction === Action.Fight) {
+            ctx.fillText('▶', 185, 450)
+        }
+        ctx.fillText('たたかう', 210, 450)
+        if (selectedAction === Action.Escape) {
+            ctx.fillText('▶', 185, 500)
+        }
+        ctx.fillText('にげる', 210, 500)
+    }
+    function draw() {
+        if (!image) { return }
+
+        const canvas = canvasRef?.current
+        const ctx = canvas?.getContext('2d')!
+        ctx.strokeStyle = 'white'
+        ctx.fillStyle = 'white'
+        ctx.lineWidth = 5
+        ctx.font = '24px sans-serif'
+
+        ctx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
+        if (gameState === GameState.cleared && controlState === ControlState.readMessage && !message) {
+            ctx.font = '50px sans-serif'
+            ctx.fillText('ゲームクリア', 250, 250)
+            ctx.fillText('へいわがおとずれた', 170, 350)
+        } else {
+            ctx.drawImage(image, 400 - 32, 230 - 32, 64, 64)
+            if (message) {
+                ctx.strokeRect(180, 400, 440, 180)
+                ctx.fillText(message, 200, 440)
+            } else {
+                drawActions(ctx, selectedAction)
+            }
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', listenKeyDown)
+        draw()
+        const image = new Image()
+        image.src = '/slime.png'
+        image.onload = () => { setImage(image) }
+
+        return () => {
+            document.removeEventListener('keydown', listenKeyDown)
+        }
+    }, [])
+
+    useEffect(() => {
+        draw();
+    }, [x, y, image, selectedAction, message]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <canvas className={"bg-black"} ref={canvasRef} id="canvas" width={view.width} height={view.height}>
+      </canvas>
     </main>
   )
 }
